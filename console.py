ad07ec05965335b04028afffe3401b06548201c1
edielam <edguard738@gmail.com>
2021-11-13 11:14:16 +0000

    fixed issues

--- console.py
+++ console.py
@@ -5,7 +5,7 @@ import cmd
 from datetime import datetime
 import models
 from models.amenity import Amenity
-from models.base_model import Base_model
+from models.BaseModel import BaseModel
 from models.city import City
 from models.place import Place
 from models.review import Review
@@ -13,7 +13,7 @@ from models.state import State
 from models.user import User
 import shlex  
 
-class_groups = {"Amenity": Amenity, "Base_model": Base_model, "City": City,
+class_groups = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
 
