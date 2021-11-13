a7c4d22f028dc0db44b4565554069e7cb85fe474
edielam <edguard738@gmail.com>
2021-11-13 10:53:25 +0000

    test_file storage

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the class TestConsoleDocs
+This contains the class Test_console_docs
 """
 
 import console
@@ -10,8 +10,8 @@ import unittest
 HBNBCommand = console.HBNBCommand
 
 
-class TestConsoleDocs(unittest.TestCase):
-    """Class for testing documentation of the console"""
+class Test_console_docs(unittest.TestCase):
+    """For testing documentation of the console"""
     def test_pep8_conformance_console(self):
         """Test that console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
