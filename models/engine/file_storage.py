6dc2f0f663ae73cb58723507c1132f8688e142fd
underdogs180 <asadirshad38@gmail.com>
2021-11-16 05:24:39 -0800

    Modified

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -49,6 +49,5 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
-            pass
-
+        "except"
+        pass
