da16edd14a56d0b863ee81a9d6951887506dd862
underdogs180 <asadirshad38@gmail.com>
2021-11-18 23:41:38 -0800

    Modified

--- console.py
+++ console.py
@@ -19,56 +19,38 @@ class HBNBCommand(cmd.Cmd):
     __my_classes = {"BaseModel",
                     "User"}
 
-     def do_EOF(self, arg):
-         """Exits Console"""
-         return True
+    def do_EOF(self, arg):
+        """Exits Console"""
+        return True
 
-     def do_quit(self, arg):
-         """Quit command to exit the program"""
-         return True
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
 
-     def emptyline(self):
-         """Overwrites the emptyline command"""
-         return False
+    def emptyline(self):
+        """Overwrites the emptyline command"""
+        return False
 
-     def do_create(self, arg):
-         """Creates a new instance of a class"""
-         if len(arg) == 0:
-             print("** class doesn't exist **")
-         elif arg not in HBNBCommand.__my_classes:
-             print("** class doesn't exist **")
-         else:
-             print(eval(arg)().id)
-             models.storage.save()
+    def do_create(self, arg):
+        """Creates a new instance of a class"""
+        if len(arg) == 0:
+            print("** class doesn't exist **")
+        elif arg not in HBNBCommand.__my_classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(arg)().id)
+            models.storage.save()
 
-     def do_show(self, arg):
-         """Prints the string representation of an instance
-         based on the class name and id
+    def do_show(self, arg):
+        """Prints the string representation of an instance
+        based on the class name and id
 
-         Usage: `$ show <class name> <id>
-         """
-         obj_dict = models.storage.all()
-
-         if len(arg) == 0:
-             print("** class doesn't exist **")
-         else:
-             tokens = arg.split()
-
-             if tokens[0] not in HBNBCommand.__my_classes:
-                 print("** class doesn't exist **")
-             elif len(tokens) == 1:
-                 print("** instance id missing **")
-             elif (tokens[0] + "." + tokens[1]) not in obj_dict:
-                 print("** no instance found **")
-             else:
-                 print(obj_dict[tokens[0] + "." + tokens[1]])
-
-    def do_destroy(self, arg):
-        """Deletes an instance based on the class name"""
+        Usage: `$ show <class name> <id>
+        """
         obj_dict = models.storage.all()
 
         if len(arg) == 0:
-            print("** class name is missing **")
+            print("** class doesn't exist **")
         else:
             tokens = arg.split()
 
@@ -76,44 +58,17 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
             elif len(tokens) == 1:
                 print("** instance id missing **")
-            elif (tokens[0] + '.' + tokens[1]) not in obj_dict:
+            elif (tokens[0] + "." + tokens[1]) not in obj_dict:
                 print("** no instance found **")
             else:
-                del obj_dict[tokens[0] + '.' + tokens[1]]
-                models.storage.save()
+                print(obj_dict[tokens[0] + "." + tokens[1]])
 
-    def do_all(self, arg):
-        """Prints all string representation of all instances
-        based or not on the classe name
-
-        Usage: `$ all <class name>`
-        """
-        obj_dict = models.storage.all()
-        selected = []
-        if len(arg) != 0:
-             if arg not in HBNBCommand.__my_classes:
-                 print("** class doesn't exist **")
-             else:
-                 for each in obj_dict.values():
-                     if arg == each.__class__.__name__:
-                         selected.append(each.__str__())
-                 print(selected)
-        else:
-            for each in obj_dict.values():
-                selected.append(each.__str__())
-            print(selected.__str__())
-
-     def do_update(self, arg):
-         """Updates an instance based on the class name and id
-         by adding or updating attribute (save the change to the JSON file
-
-         Usage: update <class name> <id> <attribute name> "<attribute value>"
-         Ex: update BaseModel 1234-1234-1234 email "aibnb@mail.com"
-         """
+     def do_destroy(self, arg):
+         """Deletes an instance based on the class name"""
          obj_dict = models.storage.all()
 
          if len(arg) == 0:
-             print("** class name missing **")
+             print("** class name is missing **")
          else:
              tokens = arg.split()
 
@@ -123,18 +78,63 @@ class HBNBCommand(cmd.Cmd):
                  print("** instance id missing **")
              elif (tokens[0] + '.' + tokens[1]) not in obj_dict:
                  print("** no instance found **")
-             elif len(tokens) == 2:
-                 print("** attribute name missing **")
-             elif len(tokens) == 3:
-                 print("** value missing **")
              else:
-                 obj = obj_dict[tokens[0] + '.' + tokens[1]]
-                 if tokens[2] in obj.__class__.__dict__.keys():
-                     v_type = type(obj.__class__.__dict__[tokens[2]])
-                     obj.__dict__[tokens[2]] = (v_type(tokens[3]))
-                 else:
-                     obj.__dict__[tokens[2]] = tokens[3]
+                 del obj_dict[tokens[0] + '.' + tokens[1]]
                  models.storage.save()
+
+     def do_all(self, arg):
+         """Prints all string representation of all instances
+         based or not on the classe name
+
+         Usage: `$ all <class name>`
+         """
+         obj_dict = models.storage.all()
+         selected = []
+         if len(arg) != 0:
+             if arg not in HBNBCommand.__my_classes:
+                 print("** class doesn't exist **")
+             else:
+                 for each in obj_dict.values():
+                     if arg == each.__class__.__name__:
+                         selected.append(each.__str__())
+                 print(selected)
+         else:
+              for each in obj_dict.values():
+                  selected.append(each.__str__())
+              print(selected.__str__())
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id
+        by adding or updating attribute (save the change to the JSON file
+
+        Usage: update <class name> <id> <attribute name> "<attribute value>"
+        Ex: update BaseModel 1234-1234-1234 email "aibnb@mail.com"
+        """
+        obj_dict = models.storage.all()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+        else:
+            tokens = arg.split()
+
+            if tokens[0] not in HBNBCommand.__my_classes:
+                print("** class doesn't exist **")
+            elif len(tokens) == 1:
+                print("** instance id missing **")
+            elif (tokens[0] + '.' + tokens[1]) not in obj_dict:
+                print("** no instance found **")
+            elif len(tokens) == 2:
+                print("** attribute name missing **")
+            elif len(tokens) == 3:
+                print("** value missing **")
+            else:
+                obj = obj_dict[tokens[0] + '.' + tokens[1]]
+                if tokens[2] in obj.__class__.__dict__.keys():
+                    v_type = type(obj.__class__.__dict__[tokens[2]])
+                    obj.__dict__[tokens[2]] = (v_type(tokens[3]))
+                else:
+                    obj.__dict__[tokens[2]] = tokens[3]
+                models.storage.save()
 
 
 if __name__ == '__main__':
